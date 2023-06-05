37fd735fbb45150f62d037031cb3e1d70eded66c
valmirdeveloper <valmirdeveloper@gmail.com>
2023-06-05 18:20:12 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var nome = document.getElementById("nome");
+var botao = document.querySelector("#botao-enviar");
+var sexo;
+var prefixo;
+
+
+
+
+    botao.addEventListener("click", (e) => {
+        if(document.getElementById("feminino").checked){
+            sexo = "menina";
+            prefixo = "uma";
+
+        }else if(document.getElementById("masculino").checked){
+            sexo = "menino";
+            prefixo = "um";  
+       
+        }
+
+        document.getElementById("mensagem").innerHTML = "Olá " + nome.value + ", " + "você é " + prefixo +" " + sexo + "?";
+    })
+
+
+   
