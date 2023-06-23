394292cbb6f2e336df4248a27b0248aa09932606
valmir augusto dos santos <92243436+valmirdeveloper@users.noreply.github.com>
2023-06-23 15:31:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var botao = document.querySelector("#botao-enviar");
 
 
 
+
     botao.addEventListener("click", (e) => {
         if(document.getElementById("feminino").checked){
             sexo = "menina";
@@ -17,8 +18,23 @@ var botao = document.querySelector("#botao-enviar");
        
         }
 
-        document.getElementById("mensagem").innerHTML = "Olá " + nome.value + ", " + "você é " + prefixo +" " + sexo + "?";
+        document.getElementById("mensagem").innerHTML = "Olá " + nome.value + ", " + "você é " + prefixo +" " + sexo + "!";
     })
 
+
+
+
+    var caixaVermelha = document.getElementById("#caixaVermelha");
+    var input = document.getElementById("#checkboxVermelho");
+
+    checkboxVermelho.addEventListener("click", (e) => {
+        if(document.getElementById("checkboxVermelho").checked){           
+
+        }
+
+        document.getElementById("caixaVermelha").style.backgroundColor = "red";
+
+        
+    })
 
    
