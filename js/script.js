f3e0e3917b2810c18f5eb42b788a13176173d418
valmir augusto dos santos <valmirdeveloper@gmail.com>
2023-06-24 14:24:22 -0300

    finalizei o javascript das caixas coloridas

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ var botao = document.querySelector("#botao-enviar");
 
         //        Caixas coloridas      //
 
-    var caixaVermelha = document.getElementById("#caixaVermelha");
+    var caixas = document.getElementById("#caixaVermelha");
     var input = document.getElementById("#checkboxVermelho");
 
     checkboxVermelho.addEventListener("click", (e) => {
@@ -33,25 +33,93 @@ var botao = document.querySelector("#botao-enviar");
         }else {
 
             document.getElementById("caixaVermelha").style.backgroundColor = "rgb(197, 189, 189)";
-        }            
-         
+        }                    
     })
 
 
 
-    var caixaAzul = document.getElementById("#caixaAzul");
+    var caixas = document.getElementById("#caixaAzul");
     var input = document.getElementById("#checkboxAzul");
 
     checkboxAzul.addEventListener("click", (e) => {
         if(document.getElementById("checkboxAzul").checked){ 
 
-            document.getElementById("caixaAzul").style.backgroundColor = "blue";
+            document.getElementById("caixaAzul").style.backgroundColor = "cyan";
 
         }else {
 
             document.getElementById("caixaAzul").style.backgroundColor = "rgb(197, 189, 189)";
-        }            
-         
+        }                     
     })
+
+
+
+    var caixas= document.getElementById("#caixaAmarela");
+    var input = document.getElementById("#checkboxAmarelo");
+
+    checkboxAmarelo.addEventListener("click", (e) => {
+        if(document.getElementById("checkboxAmarelo").checked){ 
+
+            document.getElementById("caixaAmarela").style.backgroundColor = "yellow";
+
+        }else {
+
+            document.getElementById("caixaAmarela").style.backgroundColor = "rgb(197, 189, 189)";
+        }                    
+    })
+
+
+
+    var caixas = document.getElementById("#caixaVerde");
+    var input = document.getElementById("#checkboxVerde");
+
+    checkboxVerde.addEventListener("click", (e) => {
+        if(document.getElementById("checkboxVerde").checked){ 
+
+            document.getElementById("caixaVerde").style.backgroundColor = "#15FF2C";
+
+        }else {
+
+            document.getElementById("caixaVerde").style.backgroundColor = "rgb(197, 189, 189)";
+        }                    
+    })
+
+
+
+    var caixas = document.getElementById("#caixaBranca");
+    var input = document.getElementById("#checkboxBranco");
+
+    checkboxBranco.addEventListener("click", (e) => {
+        if(document.getElementById("checkboxBranco").checked){ 
+
+            document.getElementById("caixaBranca").style.backgroundColor = "white";
+
+        }else {
+
+            document.getElementById("caixaBranca").style.backgroundColor = "rgb(197, 189, 189)";
+        }                    
+    })
+
+
+
+    var caixas = document.getElementById("#caixaPreta");
+    var input = document.getElementById("#checkboxPreto");
+
+    checkboxPreto.addEventListener("click", (e) => {
+        if(document.getElementById("checkboxPreto").checked){ 
+
+            document.getElementById("caixaPreta").style.backgroundColor = "black";
+
+        }else {
+
+            document.getElementById("caixaPreta").style.backgroundColor = "rgb(197, 189, 189)";
+        }                    
+    })
+
+
+
+
+
+
    
    
